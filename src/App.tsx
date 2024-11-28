6313fb9184da07b8f69f540df254b9dfb6b182fb
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-11-27 22:22:56 -0500

    react-navigation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>BookSwap</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;

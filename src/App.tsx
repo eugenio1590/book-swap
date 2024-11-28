3be7300411cce29b663bfcd1c8202ef7341bfdc8
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-11-28 15:32:20 -0500

    main navigator

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { adaptNavigationTheme, PaperProvider, Text } from 'react-native-paper';
+import { adaptNavigationTheme, PaperProvider } from 'react-native-paper';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import theme from './config/theme';
+import { MainNavigator } from './presentation/routes';
 
 const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme });
 
@@ -14,7 +15,7 @@ const App = () => {
       icon: props => <FeatherIcon {...props} />,
     }}>
       <NavigationContainer theme={{...LightTheme, fonts: DefaultTheme.fonts}}>
-        <Text>BookSwap</Text>
+        <MainNavigator />
       </NavigationContainer>
     </PaperProvider>
   );
---
+++ src/presentation/routes/MainNavigator.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens';
+
+export type MainNavigatorParams = {
+  Home: undefined,
+}
+
+const Stack = createStackNavigator<MainNavigatorParams>();
+
+export const MainNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainNavigator;

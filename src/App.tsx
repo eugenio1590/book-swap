cee195fddd787ba2d86b1480e92b5cede01f6ff9
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-11-27 22:51:08 -0500

    global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { PaperProvider, Text } from 'react-native-paper';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { adaptNavigationTheme, PaperProvider, Text } from 'react-native-paper';
 import FeatherIcon from 'react-native-vector-icons/Feather';
+import theme from './config/theme';
+
+const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme });
 
 const App = () => {
   return (
-    <PaperProvider settings={{
+    <PaperProvider theme={theme} settings={{
       icon: props => <FeatherIcon {...props} />,
     }}>
-      <NavigationContainer>
+      <NavigationContainer theme={{...LightTheme, fonts: DefaultTheme.fonts}}>
         <Text>BookSwap</Text>
       </NavigationContainer>
     </PaperProvider>
---
+++ src/config/theme.ts
@@ -0,0 +1,14 @@
+import { MD3LightTheme } from 'react-native-paper';
+
+const theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: '#064ACB',
+    secondary: '#F3A953',
+    tertiary: '#F27370',
+  },
+};
+
+export type AppTheme = typeof theme;
+export default theme;

64a49bf6849791b3efacc8ed2ccacbf85b22500c
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-12-02 17:10:48 -0500

    custom bottom tab navigator

--- src/presentation/routes/HomeNavigator.tsx
+++ src/presentation/routes/HomeNavigator.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text } from 'react-native';
-import { Icon } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { PlatformPressable, Text } from '@react-navigation/elements';
+import { type BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useLinkBuilder } from '@react-navigation/native';
+import { Icon, useTheme } from 'react-native-paper';
 
 interface Route {
   name: string;
@@ -20,12 +22,73 @@ const routes: Route[] = [
 
 const Tab = createBottomTabNavigator();
 
+const TabBar = ({ state, descriptors, navigation } : BottomTabBarProps) => {
+  const { colors } = useTheme();
+  const { buildHref } = useLinkBuilder();
+
+  return (
+    <View style={{...styles.tabBarContainer, backgroundColor: colors.primary}}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const tabBarIcon = options.tabBarIcon;
+        const label: any =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+              ? options.title
+              : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <PlatformPressable
+            key={index}
+            href={buildHref(route.name, route.params)}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={[styles.tabBarItem, isFocused && {...styles.tabBarButton, backgroundColor: colors.secondary}]}
+          >
+            {tabBarIcon && tabBarIcon({focused: isFocused, color: colors.onPrimary, size: 24})}
+            {isFocused && (
+              <Text style={{...styles.tabBarLabel, color: colors.onPrimary }}>
+                {label}
+              </Text>
+            )}
+          </PlatformPressable>
+        );
+      })}
+    </View>
+  );
+};
+
 export const HomeNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
+      initialRouteName={routes[0].name}
+      tabBar={(props) => <TabBar {...props} />}
+      screenOptions={{ headerShown: false }}
     >
       {routes.map((route, index) => (
         <Tab.Screen
@@ -41,5 +104,30 @@ export const HomeNavigator = () => {
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBarContainer: {
+    height: 80,
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'space-around',
+  },
+  tabBarItem: {
+    alignItems: 'center',
+  },
+  tabBarButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'space-around',
+    borderRadius: 50,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    margin: 5,
+  },
+  tabBarLabel: {
+    margin: 5,
+  },
+});
 
 export default HomeNavigator;

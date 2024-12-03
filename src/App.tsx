401ff488d84f85d5a4575db82dd930468bda916e
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-12-03 14:47:45 -0500

    icons updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import 'react-native-gesture-handler';
+import FeatherIcon from '@react-native-vector-icons/feather';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { adaptNavigationTheme, PaperProvider } from 'react-native-paper';
-import FeatherIcon from 'react-native-vector-icons/Feather';
 import theme from './config/theme';
 import { MainNavigator } from './presentation/routes';
 
@@ -12,6 +12,7 @@ const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme
 const App = () => {
   return (
     <PaperProvider theme={theme} settings={{
+      // @ts-ignore: 'name' is part of a global icon configuration and can have dynamic values.
       icon: props => <FeatherIcon {...props} />,
     }}>
       <NavigationContainer theme={{...LightTheme, fonts: DefaultTheme.fonts}}>

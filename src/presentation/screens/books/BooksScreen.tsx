9379146391b25a9978b0c2d1b21f13bfb561f76f
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-12-06 01:41:34 -0500

    book list

--- src/presentation/screens/books/BooksScreen.tsx
+++ src/presentation/screens/books/BooksScreen.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { TradeType } from 'core';
+import BookList from './components/BookList';
 
 export const BooksScreen = () => {
   return (
     <View>
-      <Text>Books</Text>
+      <BookList
+        items={[
+          {
+            id: 1,
+            user: 'Bob Burger',
+            avatar: 'https://avatars.githubusercontent.com/u/1?v=4',
+            book: 'The Great Gatsby',
+            author: 'F. Scott Fitzgerald',
+            image: 'https://example.com/gatsby.jpg',
+            trade: TradeType.buy,
+            onTradeClick: () => console.log('On trade click'),
+            onBookClick: () => console.log('On book click'),
+          },
+          {
+            id: 2,
+            user: 'Bob Smith',
+            avatar: 'https://avatars.githubusercontent.com/u/2?v=4',
+            book: '1984',
+            author: 'George Orwell',
+            image: 'https://example.com/1984.jpg',
+            trade: TradeType.buy,
+            onTradeClick: () => console.log('On trade click'),
+            onBookClick: () => console.log('On book click'),
+          },
+          {
+            id: 3,
+            user: 'Carol Davis',
+            avatar: 'https://avatars.githubusercontent.co/u/3?v=4',
+            book: 'To Kill a Mockingbird',
+            author: 'Harper Lee',
+            image: 'http://books.google.com/books/content?id=AYl1DwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+            trade: TradeType.swap,
+            onTradeClick: () => console.log('On trade click'),
+            onBookClick: () => console.log('On book click'),
+          },
+        ]}
+      />
     </View>
   );
 };
---
+++ src/presentation/screens/books/components/BookList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import { useTheme } from 'react-native-paper';
+import BookItem, { type Props as Item } from './BookItem';
+
+interface Props {
+  items: Item[]
+}
+
+export const BookList = ({items} : Props) => {
+  const theme = useTheme();
+  return (
+    <FlatList
+      style={{backgroundColor: theme.colors.surface}}
+      data={items}
+      renderItem={({item}) => <BookItem {...item} />}
+      keyExtractor={item => item.id.toString()} />
+  );
+};
+
+export default BookList;
